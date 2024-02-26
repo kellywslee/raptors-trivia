21b22afbf7e1a78501a356f678c0945243bf8a22
Kelly Lee <kellyws.lee@gmail.com>
2024-02-25 22:06:49 -0500

    Add the form to the Question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import { useTrivia } from "../hooks/useTrivia";
-import Options from "./Options";
+import Button from "./Button";
 
 export default function Question() {
-  const { questions, index } = useTrivia();
+  const { questions, index, dispatch } = useTrivia();
+  const [selectedOption, setSelectedOption] = useState("");
   const currentQuestion = questions[index];
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: "submitAnswer", payload: selectedOption });
+    setSelectedOption("");
+  };
 
   return (
     <section>
-      <h4>{currentQuestion.question}</h4>
-      <Options question={currentQuestion} />
+      <form>
+        <h3>{currentQuestion.question}</h3>
+        {currentQuestion.options.map((option, index) => (
+          <div key={index}>
+            <input
+              type="radio"
+              id={`option-${index}`}
+              name={`currentQuestion-${currentQuestion.id}-option`}
+              value={option}
+              checked={selectedOption === option}
+              onChange={handleOptionChange}
+            />
+            <label htmlFor={`option-${index}`}>{option}</label>
+          </div>
+        ))}
+        <Button text="NEXT QUESTION" onClick={handleSubmit} />
+      </form>
     </section>
   );
 }
