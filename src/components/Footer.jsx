b98847ee17fb80e4b66f498b9e2ebdfd071fdb56
Kelly Lee <kellyws.lee@gmail.com>
2024-02-27 22:01:17 -0500

    Update the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
     <footer className="py-4 text-sm">
-      Designed and Develped with ❤️ by Kelly
+      Designed and Develped with ❤️{" "}
+      <a
+        href="https://www.kellyws.me"
+        target="_blank"
+        rel="noreferrer"
+        className="hover:font-bold hover:underline"
+      >
+        Kelly Lee
+      </a>{" "}
+      2024
     </footer>
   );
 }
